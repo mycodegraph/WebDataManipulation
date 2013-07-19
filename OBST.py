a88a6779b3b6433a307ad1a5b7871eb573df3b50
Ankur Gupta <gm.ankurgarg@gmail.com>
2013-07-19 14:20:55 +0530

    Update OBST.py

--- OBST.py
+++ OBST.py
@@ -1,3 +1,17 @@
+def sum( sum_arr, i, j):
+	if sum_arr[i][j] > 0:
+		return sum_arr[i][j]
+	m = 0
+	sum=0
+	for k,v in word_freq_hash.iteritems():
+		m+=1	
+		if m >= i:
+			sum+=v
+			if(m==j):
+				sum_arr[i][j] = sum
+				return sum		
+		
+			
 		
 		
 def create_optimal_bst(n):
@@ -22,7 +36,7 @@ def create_optimal_bst(n):
 			cost[i][j] = 1000000
 			
 			for r in range(i,j+1):
-				c = (cost[i][r-1] if ( i < r ) else 0) + (cost[r+1][j] if ( j > r ) else 0) + sum( sum_arr, i, j )
+				c = cost[i][r-1] if ( i < r ) else 0 + cost[r+1][j] if ( j > r ) else 0 + sum( sum_arr, i, j )
 				if c < cost[i][j]:
 					cost[i][j] = c
 					root[i][j] = r
@@ -32,17 +46,5 @@ def create_optimal_bst(n):
 			print(str(root[row][col])+" "),
 			#print str(cost[row][col])+" "
 		print "\n"
+			
 
-def sum( sum_arr, i, j):
-  if sum_arr[i][j] > 0:
-		return sum_arr[i][j]
-	m = 0
-	sum=0
-	for k,v in word_freq_hash.iteritems():
-		m+=1	
-		if m >= i:
-			sum+=v
-			if(m==j):
-				sum_arr[i][j] = sum
-				return sum		
-		
