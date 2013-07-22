7d0ad6d6b9d4b5865ca838a0d0de30a73753fccc
Ankur Gupta <gm.ankurgarg@gmail.com>
2013-07-22 15:26:18 +0530

    Update FreqTable.py

--- FreqTable.py
+++ FreqTable.py
@@ -33,6 +33,9 @@ def prepare_list_amazon(file, soup):
 			
 	
 def crawl(file, addr):
+	proxy_support = urllib2.ProxyHandler({"http":"http://xxx.xxx.xx.xx:80"}) #Proxy address
+	opener = urllib2.build_opener(proxy_support)
+	urllib2.install_opener(opener)
 	html = urllib2.urlopen(addr).read();
 	prepare_list_amazon(file, BeautifulSoup(html))
 
@@ -70,9 +73,9 @@ if __name__ == "__main__":
 	
 	
 	m=0
-	while m != n:
+	while m <= n:
 		#Creating 10 threads to fetch network data and print it to the file
-		for t in range(1, 20) :
+		for t in range(1, 10) :
 			addr = temp + str(m) + last
 			thread  = threading.Thread(target = crawl, args = (file, addr, ))
 			thread.start()
@@ -81,8 +84,9 @@ if __name__ == "__main__":
 			print "\nFrom:Thread: " + str(t) + " and Web Address: " + addr
 			print "Waiting"	
 			m+=1
-			if(m == n):
+			if(m > n):
 				break	
+			
 		#joining threads; although not required but generally with so many threads fetching data from amazon server,
 		#leads to "Server blocking request"	
 		for thread in Threads:
@@ -92,9 +96,31 @@ if __name__ == "__main__":
 			
 			
 	for k,v in word_freq_hash.iteritems():
+		freqFile.write(" %-45s %-15s %15s\n" % (k, "=>", str(v)))	
+		
+	# Thid code calculates the eidt distance vallue for all strings in frequency table against the input string and 
+	#prints the one with least "edit distance"
+	string1 = raw_input()	
+	min = 100000
+	temp=" "
+	for k,v in word_freq_hash.iteritems():
 		#print " %-45s %-15s %15s" % (k, "=>", str(v))
-		freqFile.write(" %-45s %-15s %15s\n" % (k, "=>", str(v)))
+		
+		string2 = k
+		matrix = []
+		val=0
+		
+		for row in range(0,len(string1)+1):
+			matrix.append([0 for _ in range(0,len(string2)+1)])
+
+		val =  edit_cost(string1, string2, len(string1)-1, len(string2)-1,matrix)
+		if val < min:
+			temp = string2
+			min = val
+				
+	print temp +" "+str(min)
 	
+	#--------------------------------------------------------->
 		
 	#create_optimal_bst(len(word_freq_hash))
 	print time.clock() - start_time
